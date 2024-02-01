13e8cff52855edf26ab8a976fc803015c3ad9259
anhtruyenb5 <anhtruyenb5@gmail.com>
2024-02-01 15:35:19 +0700

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './myapp/Header';
+import Banner from './myapp/Banner';
+import Item from './myapp/Item';
+import Footer from './myapp/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div>
+        <Banner />
+        <Item />
+      </div>
+      <Footer />
     </div>
   );
 }
